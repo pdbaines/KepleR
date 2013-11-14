d94c53410f91be0d4659a2f1a79e1f3623fca442
christopheraden <christopher.b.aden@gmail.com>
2013-11-13 20:19:07 -0800

    Add a few more plots, finish up AR(1) simulated datasets

--- python-bls/plot_bls.py
+++ python-bls/plot_bls.py
@@ -9,37 +9,48 @@ signals = np.load("signals.npy")
 noises = np.load("noises.npy")
 
 diffs = np.subtract(bls_periods, true_periods)
+diffs_rel = 100 * np.divide(diffs, np.abs(true_periods))
 SNR = np.divide(signals, np.sqrt(noises))
-
-hi_SNR = SNR[SNR > 2]
-hi_diff = diffs[SNR > 2]
-lo_SNR = SNR[SNR < 2]
-lo_diff = diffs[SNR < 2]
-
-diffs_rel = np.divide(diffs, np.abs(true_periods))
 
 plt.close('all')
 subplot(221)
 plt.scatter(SNR, diffs)
 plt.ylabel("Estimated Period-True Period")
 plt.xlabel("Signal to Noise Ratio")
-plt.title("SNR v. Period Difference")
 
 subplot(222)
-plt.scatter(SNR, diffs_rel)
-plt.ylabel("Est. Period - True Period / |True|")
-plt.xlabel("Signal to Noise Ratio")
-
-subplot(223)
-plt.scatter(lo_SNR, lo_diff)
-plt.ylabel("Estimated Period-True Period")
-plt.xlabel("Signal to Noise Ratio < 2")
-
-subplot(224)
-plt.scatter(hi_SNR, hi_diff)
+plt.scatter(SNR[SNR > 2], diffs[SNR > 2])
 plt.ylabel("Estimated Period-True Period")
 plt.xlabel("Signal to Noise Ratio > 2")
+plt.xlim([2.0, 8.0])
 
+ax2 = plt.subplot2grid((2, 2), (1, 0), colspan=2)
+plt.scatter(SNR[SNR < 2], diffs[SNR < 2])
+plt.ylabel("Estimated Period-True Period")
+plt.xlabel("Signal to Noise Ratio < 2")
+plt.xlim([0.0, 2.0])
 plt.tight_layout()
+plt.savefig('SNR_vs_AbsDiff.pdf')
 
-plt.savefig('SNR_vs_Diff.pdf')
+
+#Relative Differences now
+plt.close('all')
+subplot(221)
+plt.scatter(SNR, diffs_rel)
+plt.ylabel("Relative Error in Period (%)")
+plt.xlabel("Signal to Noise Ratio")
+
+subplot(222)
+plt.scatter(SNR[SNR > 2], diffs_rel[SNR > 2])
+plt.ylabel("Relative Error in Period (%)")
+plt.xlabel("Signal to Noise Ratio > 2")
+plt.xlim([2.0, 8.0])
+plt.ylim([-.1, .1])
+
+ax2 = plt.subplot2grid((2, 2), (1, 0), colspan=2)
+plt.scatter(SNR[SNR < 2], diffs_rel[SNR < 2])
+plt.ylabel("Relative Error in Period (%)")
+plt.xlabel("Signal to Noise Ratio < 2")
+plt.xlim([0.0, 2.0])
+plt.tight_layout()
+plt.savefig('SNR_vs_RelDiff.pdf')
