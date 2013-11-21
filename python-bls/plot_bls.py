ac20f48f8b1ae0fca19888921a3452fb8480c35a
christopheraden <christopher.b.aden@gmail.com>
2013-11-20 23:27:41 -0800

    Last of the plot edits

--- python-bls/plot_bls.py
+++ python-bls/plot_bls.py
@@ -10,6 +10,8 @@ true_signal = np.load("../Results/true_signal.npy")
 bls_signal = np.load("../Results/bls_depth.npy")
 
 true_td = np.load("../Results/true_td.npy")
+true_q = true_td/true_period
+
 bls_q = np.load("../Results/bls_q.npy")
 bls_td = np.multiply(bls_q, bls_period)
 
@@ -65,20 +67,20 @@ def plot_var(variable, snr=true_snr):
     plt.savefig('../Results/Graphs/SNR_vs_RelDiff' + variable.title() + '.pdf')
     return 0
 
-#plot_var("period")
-#plot_var("signal")
-#plot_var("td")
-#plot_var("t0")
+plot_var("period")
+plot_var("signal")
+plot_var("td")
+plot_var("t0")
 
 plt.close('all')
-subplot(221)
+subplot(311)
 plt.scatter(true_period[true_snr>2], bls_period[true_snr>2])
 plt.ylabel("Estimated Period")
 plt.xlabel("True Period (SNR > 2)")
 plt.xlim([8000.0, 20000.0])
 plt.ylim([8000.0, 20000.0])
 
-subplot(222)
+subplot(312)
 plt.scatter(true_signal[true_snr>2], bls_signal[true_snr>2])
 plt.ylabel("Estimated Signal")
 plt.xlabel("True Signal (SNR > 2)")
@@ -86,24 +88,31 @@ plt.xlim([0.025, 0.085])
 plt.ylim([0.025, 0.085])
 
 
-subplot(223)
+subplot(313)
 plt.scatter(true_td[true_snr>2], bls_td[true_snr>2])
 plt.ylabel("Estimated Transit Duration")
 plt.xlabel("True Transit Duration (SNR > 2)")
 plt.xlim([40.0, 100.0])
 plt.ylim([40.0, 100.0])
+plt.tight_layout()
+#plt.show()
+plt.savefig('../Results/Graphs/True_vs_Est1.pdf', dpi=gcf().dpi)
 
-
-subplot(224)
+plt.close('all')
+subplot(211)
 plt.scatter(true_t0[true_snr>2], bls_t0[true_snr>2])
 plt.ylabel("Estimated Time to First Transit")
 plt.xlabel("True Time to First Transit (SNR > 2)")
 plt.xlim([0.0, 20000.0])
 plt.ylim([0.0, 20000.0])
 
+subplot(212)
+plt.scatter(true_q[true_snr>2], bls_q[true_snr>2])
+plt.ylabel("Estimated Fractional Transit Duration")
+plt.xlabel("True Fractional Transit Duration (SNR > 2)")
+plt.xlim([0.002, 0.01])
+plt.ylim([0.002, 0.01])
 
 plt.tight_layout()
-plt.show()
-
-
-#plt.savefig('../Results/Graphs/True_vs_Est.png')
+#plt.show()
+plt.savefig('../Results/Graphs/True_vs_Est2.pdf', dpi=gcf().dpi)
